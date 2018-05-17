f7fa8ab63f8e9712758a771bf8b483d0db940780
git <nismit.dev@gmail.com>
2018-05-16 23:39:48 -0700

    Build - WIP

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 import * as bufferSpace from './bufferSpace';
 import * as shaderSpace from './shaderSpace';
+import * as fogParticle from './fogParticle';
 
 const init = () => {
   // bufferSpace.default();
-  shaderSpace.default();
+  // shaderSpace.default();
+  fogParticle.default();
 }
 
 window.addEventListener('load', init);
